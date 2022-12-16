6eca33ad65d06c665badb7c3d9e5ad2c32196cea
Amit Sharma <amit@neobrix.io>
2022-12-16 15:54:53 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+function onButtonClick() {
+  window.open("amitdeeplink://amitdeeplink.com");
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={onButtonClick}>Click</button>
     </div>
   );
 }
