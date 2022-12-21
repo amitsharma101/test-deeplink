a021b87adbac984a437805c7bd1b4ad896783c98
Amit Sharma <amit@neobrix.io>
2022-12-21 16:00:20 +0530

    added auth0 google lock

--- src/App.js
+++ src/App.js
@@ -5,8 +5,20 @@ function App() {
 
 function onButtonClick() {
   // window.location.href = "amitdeeplink://amitdeeplink.com"
-  window.location = "amitdeeplink://amitdeeplink.com"
+  // window.location = "amitdeeplink://amitdeeplink.com"
   // window.open("amitdeeplink://amitdeeplink.com");
+
+  // var webAuth = new window.auth0.WebAuth({
+  //   domain:       'neobrix-gamepay.us.auth0.com',
+  //   clientID:     'QclomwNvig31IxmYN4URxlo8ECTO0A4G',
+  //   responseType: 'token',
+  //   redirectUri: 'amitdeeplink://amitdeeplink.com',
+  // });
+
+  // // Trigger login with google
+  // webAuth.authorize({
+  //   connection: 'google-oauth2'
+  // });
 }
 
   return (
