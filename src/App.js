d8f96595728485248b2d59e2575074623c016643
Amit Sharma <amit@neobrix.io>
2022-12-22 12:19:47 +0530

    updated redirect link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import About from "./About.js";
+import { BrowserRouter } from 'react-router-dom'
+import auth0 from 'auth0-js';
 
 function App() {
 
@@ -8,23 +12,32 @@ function onButtonClick() {
   // window.location = "amitdeeplink://amitdeeplink.com"
   // window.open("amitdeeplink://amitdeeplink.com");
 
-  // var webAuth = new window.auth0.WebAuth({
-  //   domain:       'neobrix-gamepay.us.auth0.com',
-  //   clientID:     'QclomwNvig31IxmYN4URxlo8ECTO0A4G',
-  //   responseType: 'token',
-  //   redirectUri: 'amitdeeplink://amitdeeplink.com',
-  // });
+  var webAuth = new auth0.WebAuth({
+    domain:       'neobrix-gamepay.us.auth0.com',
+    clientID:     'QclomwNvig31IxmYN4URxlo8ECTO0A4G',
+    responseType: 'token',
+    redirectUri: 'amitdeeplink://amitdeeplink.com',
+  });
 
-  // // Trigger login with google
-  // webAuth.authorize({
-  //   connection: 'google-oauth2'
-  // });
+  // Trigger login with google
+  webAuth.authorize({
+    connection: 'google-oauth2'
+  });
 }
 
   return (
+    <>
     <div className="App">
       <button onClick={onButtonClick}>Click</button>
     </div>
+
+    <BrowserRouter>
+      <Routes>
+          <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+
+    </>
   );
 }
 
